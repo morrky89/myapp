36b55aeb43b313ffdc26e264d117e8241bad8aa9
marcin-nitr <wiercioch1989@gmail.com>
2015-03-17 14:30:13 +0000

    autentyfikacja

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
+ 
+  # snipped for brevity
   protect_from_forgery with: :exception
 end
