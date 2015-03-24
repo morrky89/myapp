ffbe3680ca9dca8a1162eac16edcb3b4e685d00a
marcin-nitr <wiercioch1989@gmail.com>
2015-03-24 14:14:15 +0000

    Added log in sign out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324133448) do
+ActiveRecord::Schema.define(version: 20150324140909) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150324133448) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
