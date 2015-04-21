10f3bef56fbb9185dfb1126c3222e4f8df79458d
marcin-nitr <wiercioch1989@gmail.com>
2015-04-21 12:37:25 +0000

    article new tests

--- spec/controllers/articles_controller.spec.rb
+++ spec/controllers/articles_controller.spec.rb
@@ -13,10 +13,24 @@ describe ArticlesController do
   end
   
   describe 'show' do
-    it 'return article' do
-      get :index
+    it 'return a article' do
+      get :show, id: @article.id
       
       expect(response).to have_http_status(:success)
+      expect(assigns(:article)).not_to eq nil
+      expect(assigns(:article).title).to eq @article.title
+
+    end
+  end
+  
+  describe 'new' do
+    it 'return a new article' do
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in FactoryGirl.create[:admin]
+      get :new
+      
+      expect(response).to have_http_status(:success)
+      expect(assigns(:article)).not_to eq nil
     end
   end
 end
